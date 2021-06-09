3c7c9b8623416bee6110eefd8819adbaff7f8f37
leodzung <42915398+leodzung@users.noreply.github.com>
2021-06-09 18:01:15 -0400

    Update 834 - sumDistancesTree.py
    Update function names

--- LeetCode/ShortestPath/834 - sumDistancesTree.py
+++ LeetCode/ShortestPath/834 - sumDistancesTree.py
@@ -8,22 +8,22 @@ def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
         ans = [0] * n
         
         # Calculate the answer for the root, which we assign to the first node
-        def DFS(node=0, parent=None):
+        def BottomUpDFS(node=0, parent=None):
             for child in graph[node]:
                 if child != parent:
                     # Postorder DFS to calculate bottom up
-                    DFS(child, node)
+                    BottomUpDFS(child, node)
                     count[node] += count[child]
                     ans[node] += ans[child] + count[child]
                     
         # With the correct answer for the root, calculate the answers for its descendants
-        def DFS2(node=0, parent=None):
+        def TopDownDFS(node=0, parent=None):
             for child in graph[node]:
                 if child != parent:
                     # Preorder DFS to calculate top down
                     ans[child] = (ans[node] - count[child]) + (n - count[child])
-                    DFS2(child, node)
+                    TopDownDFS(child, node)
                     
-        DFS()
-        DFS2()
+        BottomUpDFS()
+        TopDownDFS()
         return ans
